81db53120f9e7b669cf441016a1ebd1582082b55
SeeddheeCodes <seeddhee212@gmail.com>
2025-02-12 16:18:29 -0800

    Added the Logo and basic template as well as images

--- vue-widget/src/App.vue
+++ vue-widget/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="guidance-widget">
+    <!-- Logo Section -->
+    <div class="widget-header">
+      <img src="/logo.png" alt="Widget Logo" class="widget-logo" />
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.guidance-widget {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.widget-header {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+}
+
+.widget-logo {
+  height: 40px;
+  width: auto;
 }
 </style>
